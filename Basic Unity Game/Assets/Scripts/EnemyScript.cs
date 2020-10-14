94e6cb4376a03bcb208b9bcec077278e8a90b0a2
Rhys <rhys.gale@symphonyretailai.com>
2020-10-14 20:30:00 +0100

    Enemy death particle system

--- Basic Unity Game/Assets/Scripts/EnemyScript.cs
+++ Basic Unity Game/Assets/Scripts/EnemyScript.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         BirdScript birdCollision = collision.collider.GetComponent<BirdScript>();
 
         if (birdCollision != null)
         {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
 
@@ -19,6 +22,9 @@ public class EnemyScript : MonoBehaviour
 
         var hitOnHead = collision.contacts[0].normal.y < -0.5;
         if (hitOnHead)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
+        }
     }
 }
