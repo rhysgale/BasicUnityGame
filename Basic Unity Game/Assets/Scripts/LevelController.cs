4e1031edaa12ec012e08a0a2775f2a29f39a2a9a
Rhys <rhys.gale@symphonyretailai.com>
2020-10-14 20:40:35 +0100

    Level manager with 2 levels

---
+++ Basic Unity Game/Assets/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private EnemyScript[] _enemies;
+    private static int _nextLevelIndex = 1;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<EnemyScript>();
+    }
+
+    void Update()
+    {
+        foreach(var enemy in _enemies)
+        {
+            if (enemy != null)
+            {
+                return;
+            }
+        }
+
+        //All enemies have been killed
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
+    }
+}
