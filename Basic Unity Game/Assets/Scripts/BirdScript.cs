987e1d75ab892a73ad6b3e591da46d805795d947
Rhys <rhys.gale@symphonyretailai.com>
2020-10-14 19:20:57 +0100

    Level tiles + Launchable bird

---
+++ Basic Unity Game/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdScript : MonoBehaviour
+{
+    private Vector3 _startingPosition;
+    private bool _birdWasLaunched;
+    private float _timeSittingAround;
+
+    [SerializeField] private float _launchPower;
+
+    private void Awake()
+    {
+        _startingPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1f)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 20 ||
+            transform.position.y < -20 ||
+            transform.position.x > 20 ||
+            transform.position.x < -20 ||
+            _timeSittingAround > 3)
+        {
+            var currentSceneName = SceneManager.GetActiveScene().name;
+
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.color = Color.red;
+    }
+
+    public void OnMouseUp()
+    {
+        var directionToInitialPosition = _startingPosition - transform.position;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        var ridgidBody = GetComponent<Rigidbody2D>();
+
+        spriteRenderer.color = Color.white;
+        ridgidBody.AddForce(directionToInitialPosition * _launchPower);
+        ridgidBody.gravityScale = 1;
+        _birdWasLaunched = true;
+    }
+
+    public void OnMouseDrag()
+    {
+        var newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = transform.position.z;
+
+        transform.position = newPos;
+    }
+}
